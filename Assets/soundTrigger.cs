b275353835b13fc1ba8780c54d5335576d2c9956
sandhrarahardjo <sandhra.rahardjo@binus.ac.id>
2024-02-07 12:27:19 +0700

    First Commit
    -Add Ball
    -Add Rigidbody
    -Add Bounce
    -Add soundTrigger

---
+++ Assets/soundTrigger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundTrigger : MonoBehaviour
+{
+  private void onTriggerEnter(Collider other)
+  {
+    print("kena");
+  }
+}
